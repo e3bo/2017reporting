7e9d5dd51c3a243ead60b7dac85a8a919d74b36c
Eamon O'Dea <odea35@gmail.com>
2017-12-12 17:11:00 -0500

    Simplify expression for sfm_reports_nb_eq

--- src/moment-equations.R
+++ src/moment-equations.R
@@ -8,7 +8,7 @@ var_nb_eq <- expression(math_mean +
 var_binom_eq <- expression(math_mean ^ 2 * math_sfm_cases + math_mean -
                              math_mean ^ 2)
 sfm_reports_nb_eq <- expression((1 + 1 / phi) *
-    (math_mean * (math_mean * math_sfm_cases + xi)) / math_mean ^ 2)
+    (math_sfm_cases + xi / math_mean))
 bilinear_mom_eq <- expression(1 +
     lambda / (gamma^2 * nu) * sinh(gamma) ^ 2 * exp(-(eta - lambda) * tau))
 autocor_eq <- expression((math_bmf - 1) * math_mean ^ 2 / math_var)
