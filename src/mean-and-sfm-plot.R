a3a2cea48614959465724dac8ef0266891ab0cdf
Eamon O'Dea <odea35@gmail.com>
2018-07-19 13:48:00 -0400

    Add other ews to good and bad contrast plot

--- src/mean-and-sfm-plot.R
+++ src/mean-and-sfm-plot.R
@@ -7,20 +7,27 @@ library(tidyr)
 source("moment-equations.R")
 source("plotting.R")
 
-pgrid <- expand.grid(xi = seq(0, 1, by = 0.1),
+pgrid <- expand.grid(xi = seq(0, 1, by = 0.001),
                      lambda = c(0.1, 0.5, 0.9))
 pgrid$nu <- 1
 pgrid$T <- 1
 pgrid$eta <- 1
-pgrid$mean <- eval_eq(pgrid, mean_eq)
+pgrid$math_mean <- eval_eq(pgrid, mean_eq)
 pgrid$gamma <- eval_eq(pgrid, gamma_eq)
-pgrid$second_factorial_mom <- eval_eq(pgrid, sfm_cases_eq)
+pgrid$math_sfm_cases <- eval_eq(pgrid, sfm_cases_eq)
+pgrid$math_var <- eval_eq(pgrid, var_binom_eq)
+pgrid$math_cv <- eval_eq(pgrid, cv_binom_eq)
+pgrid$tau <- pgrid$T
+pgrid$math_bmf <- eval_eq(pgrid, bilinear_mom_eq)
+pgrid$math_autocor <- eval_eq(pgrid, autocor_eq)
+
 pgrid %>%
-  gather(mean, second_factorial_mom, key = "moment", value = "value") -> pdata
+  gather(math_mean, math_sfm_cases, math_var, math_cv, math_autocor,
+         key = "moment", value = "value") -> pdata
 
 pdata$moment <- factor(pdata$moment,
-                       labels = c("Mean", "Second factorial moment"),
-                       levels = c("mean", "second_factorial_mom"))
+                       labels = c("Mean", "Second factorial moment", "Variance", "Coefficient of variation", "Lag-1 autocorrelation"),
+                       levels = c("math_mean", "math_sfm_cases", "math_var", "math_cv", "math_autocor"))
 pdata$lambda_factor <- as.factor(pdata$lambda)
 
 g <- ggplot(pdata,
@@ -35,7 +42,7 @@ leglab <- expression(paste("Trans. rate, ", lambda ~ (wk^{-1})))
 g <- g + labs(x = expression(paste("Reporting probability, ", xi)),
               y = "Value", color = leglab, size = leglab)
 
-ggsave(file = "mean-and-sfm.pdf", plot = g, height = 84, width = 84,
+ggsave(file = "mean-and-sfm.pdf", plot = g, height = 175, width = 84,
        units = "mm")
 
 sessionInfo()
